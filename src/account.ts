ace564a8f8d9e9ea7e6672745022cbb7f396f2ed
Austin King <austin@stratalabs.io>
2018-09-06 00:56:54 -0700

    fix: space linting

--- src/account.ts
+++ src/account.ts
@@ -1,178 +1,197 @@
 import BigNumber from 'bignumber.js'
-import BtcPlugin = require ('.')
+import BtcPlugin = require('.')
 const BtpPacket = require('btp-packet')
 import * as IlpPacket from 'ilp-packet'
-import { DataHandler, MoneyHandler } from './utils/types'
-import { promisify } from 'util'
-import { randomBytes } from 'crypto'
-import { BtpPacket, BtpPacketData, BtpSubProtocol } from 'ilp-plugin-btp'
-import { validate as validateAddress } from 'bitcoin-address'
-import { Channel } from './utils/lightning-types'
+import {
+  DataHandler,
+  MoneyHandler
+} from './utils/types'
+import {
+  promisify
+} from 'util'
+import {
+  randomBytes
+} from 'crypto'
+import {
+  BtpPacket,
+  BtpPacketData,
+  BtpSubProtocol
+} from 'ilp-plugin-btp'
+import {
+  validate as validateAddress
+} from 'bitcoin-address'
+import {
+  Channel
+} from './utils/lightning-types'
 
 // Used to denominate which assetScale we are using
-export enum Unit { BTC = 9, Satoshi = 0 }
+export enum Unit {
+  BTC = 9, Satoshi = 0
+}
 
 // Used to track need to settle
-enum SettleState { NotSettling, Settling, QueuedSettle }
+enum SettleState {
+  NotSettling,
+  Settling,
+  QueuedSettle
+}
 
 // Simple conversion for BTC <-> Satoshi
-export const convert = (num:BigNumber.Value, from: Unit, to: Unit): BigNumber =>
-	new BigNumber(num).shiftedBy(from - to)
+export const convert = (num: BigNumber.Value, from: Unit, to: Unit): BigNumber =>
+  new BigNumber(num).shiftedBy(from - to)
 
 // Prints our debug info nicely
 export const format = (num: BigNumber.Value, from: Unit) =>
-	convert(num, from, Unit.Satoshi) + ' BTC'
+  convert(num, from, Unit.Satoshi) + ' BTC'
 
-export const requestId = async () =>
-	(await promisify(randomBytes)(4)).readUInt32BE(0)
+export const requestId = async() =>
+  (await promisify(randomBytes)(4)).readUInt32BE(0)
 
 export default class BtcAccount {
-	// private master: LndPlugin
-	private account: { 
-		settling: SettleState
-		isBlocked: boolean
-		accountName: string
-		balance: BigNumber
-		address?: string
-		channelId?: string
-	}
-	private master: BtcPlugin
-	private sendMessage: (message: BtpPacket) => Promise<BtpPacketData>	
-	constructor( opts: {
-		accountName: string,
-		master: BtcPlugin,
-		sendMessage: (message: BtpPacket) => Promise<BtpPacketData>
-	}) {
-		this.account = {
-			settling: SettleState.NotSettling,
-			isBlocked: false,
-			accountName: opts.accountName,
-			balance: new BigNumber(0)
-		}
-		this.master = opts.master
-		this.sendMessage = opts.sendMessage
-	}
+  // private master: LndPlugin
+  private account: {
+    settling: SettleState
+    isBlocked: boolean
+    accountName: string
+    balance: BigNumber
+    address ? : string
+    channelId ? : string
+  }
+  private master: BtcPlugin
+  private sendMessage: (message: BtpPacket) => Promise < BtpPacketData >
+    constructor(opts: {
+      accountName: string,
+      master: BtcPlugin,
+      sendMessage: (message: BtpPacket) => Promise < BtpPacketData >
+    }) {
+      this.account = {
+        settling: SettleState.NotSettling,
+        isBlocked: false,
+        accountName: opts.accountName,
+        balance: new BigNumber(0)
+      }
+      this.master = opts.master
+      this.sendMessage = opts.sendMessage
+    }
 
-	addBalance(amount: BigNumber) {
-		if (amount.isZero()) return
-		if (amount.lt(0)) throw new Error('cannot add negative amount to balance')
- 
-		const maximum = this.master._balance.maximum
-		const newBalance = this.account.balance.plus(amount)
+  addBalance(amount: BigNumber) {
+    if (amount.isZero()) return
+    if (amount.lt(0)) throw new Error('cannot add negative amount to balance')
 
-		if (newBalance.gt(maximum)) {
-			throw new Error(`Cannot debit ${format(amount, Unit.Satoshi)} from account ${this.account.accountName}, ` + 
-			  `proposed balance of ${format(newBalance, Unit.Satoshi)} exceeds maximum of ${format(maximum, Unit.Satoshi)}`)
-		}
+    const maximum = this.master._balance.maximum
+    const newBalance = this.account.balance.plus(amount)
 
-		this.master._log.trace(`Debited ${format(amount, Unit.Satoshi)} from account ${this.account.accountName}, new balance is ${format(newBalance, Unit.Satoshi)}`)
-	}
+    if (newBalance.gt(maximum)) {
+      throw new Error(`Cannot debit ${format(amount, Unit.Satoshi)} from account ${this.account.accountName}, ` +
+        `proposed balance of ${format(newBalance, Unit.Satoshi)} exceeds maximum of ${format(maximum, Unit.Satoshi)}`)
+    }
 
-	subBalance(amount: BigNumber) { 
-		if (amount.isZero()) return
-		if (amount.lt(0)) throw new Error('cannot subtract negative amount from balance')  
+    this.master._log.trace(`Debited ${format(amount, Unit.Satoshi)} from account ${this.account.accountName}, new balance is ${format(newBalance, Unit.Satoshi)}`)
+  }
 
-		const minimum = this.master._balance.minimum
-		const newBalance = this.account.balance.minus(amount)
+  subBalance(amount: BigNumber) {
+    if (amount.isZero()) return
+    if (amount.lt(0)) throw new Error('cannot subtract negative amount from balance')
 
-		if (newBalance.lt(minimum)) {
-			throw new Error(`Cannot credit ${format(amount, Unit.Satoshi)} to account ${this.account.accountName}, ` + 
-				`proposedBalance of ${format(newBalance, Unit.Satoshi)} is below the minimum of ${format(minimum, Unit.Satoshi)}`)
+    const minimum = this.master._balance.minimum
+    const newBalance = this.account.balance.minus(amount)
 
-			this.master._log.trace(`Credited ${format(amount, Unit.Satoshi)} to account ${this.account.accountName}, ` + ` new balance is ${format(newBalance, Unit.Satoshi)}`)
-				this.account.balance = newBalance
-		}
-	}
-	
-	async connect () {
-		const savedAccount = await this.master._store.loadObject(`account:${this.account.accountName}`)
+    if (newBalance.lt(minimum)) {
+      throw new Error(`Cannot credit ${format(amount, Unit.Satoshi)} to account ${this.account.accountName}, ` +
+        `proposedBalance of ${format(newBalance, Unit.Satoshi)} is below the minimum of ${format(minimum, Unit.Satoshi)}`)
 
-		this.account = new Proxy({
-			...this.account,
-			...savedAccount
-		}, {
-			set: (account, key, val) => {
-				this.master._store.set(this.account.accountName, JSON.stringify({
-					...account,
-					[key]: val
-				}))
-				return Reflect.set(account, key, val)
-			}
-		})
-	}
+      this.master._log.trace(`Credited ${format(amount, Unit.Satoshi)} to account ${this.account.accountName}, ` + ` new balance is ${format(newBalance, Unit.Satoshi)}`)
+      this.account.balance = newBalance
+    }
+  }
 
-	async shareAddress (): Promise<void> {
-		const response = await this.sendMessage({
-			type: BtpPacket.TYPE_MESSAGE,
-			requestId: await requestId(),
-			data: {
-				protocolData: [{
-					protocolName: 'info',
-					contentType: BtpPacket.MIME_APPLICATION_JSON,
-					data: Buffer.from(JSON.stringify({
-						ethereumAddress: this.master._address
-					}))
-				}]
-			}
-		})
-	}
+  async connect() {
+    const savedAccount = await this.master._store.loadObject(`account:${this.account.accountName}`)
 
-	linkAddress (info: BtpSubProtocol): void {
-		const { address } = JSON.parse(info.data.toString()) 
+    this.account = new Proxy({
+      ...this.account,
+      ...savedAccount
+    }, {
+      set: (account, key, val) => {
+        this.master._store.set(this.account.accountName, JSON.stringify({
+          ...account,
+          [key]: val
+        }))
+        return Reflect.set(account, key, val)
+      }
+    })
+  }
 
-		if (this.account.address) {
-			/* for some reason Kincaid's ETH version checks: 
-			 * if (this.account.address.toLowerCase === address.toLowerCase()) :
-			 * return True
-			 * Not sure why that makes a difference if they don't match because he
-			 * logs the message this address is already linked if not, so I'm not sure
-			 * what the lowercasing has to do with why we would instead do nothing
-			 * instead of logging that it already exists */
-			return this.master._log.trace(`Cannot link BTC address ${address} to account ` + 
-				`${this.account.accountName}: ${this.account.address} is already linked`)
-		}
+  async shareAddress(): Promise < void > {
+    const response = await this.sendMessage({
+      type: BtpPacket.TYPE_MESSAGE,
+      requestId: await requestId(),
+      data: {
+        protocolData: [{
+          protocolName: 'info',
+          contentType: BtpPacket.MIME_APPLICATION_JSON,
+          data: Buffer.from(JSON.stringify({
+            ethereumAddress: this.master._address
+          }))
+        }]
+      }
+    })
+  }
 
-		if (validateAddress(address)) {
-			this.account.address = address
-			this.master._log.trace(`Successfully linked address ${address} to account ${this.account.accountName}`)
-		} else {
-			this.master._log.trace(`Failed to link address: ${address} is not a valid address`)
-		}
-	}
+  linkAddress(info: BtpSubProtocol): void {
+    const {
+      address
+    } = JSON.parse(info.data.toString())
 
-	async fundOutgoingChannel (settlementBudget: BigNumber): Promise<BigNumber> {
-		let requiresNewChannel = false
-		let requiresDeposit = false
-		let channel: Channel | null
+    if (this.account.address) {
+      /* for some reason Kincaid's ETH version checks: 
+       * if (this.account.address.toLowerCase === address.toLowerCase()) :
+       * return True
+       * Not sure why that makes a difference if they don't match because he
+       * logs the message this address is already linked if not, so I'm not sure
+       * what the lowercasing has to do with why we would instead do nothing
+       * instead of logging that it already exists */
+      return this.master._log.trace(`Cannot link BTC address ${address} to account ` +
+        `${this.account.accountName}: ${this.account.address} is already linked`)
+    }
 
-		// Check if channel already exists
-		if (typeof(this.account.channel) === 'string') {
-			channel = await this._getChannel()
-			// if channel does not exist, just create a new one
-			if (!channel) return lndlib.createChannel()
-			if (settlementBudget.gt(channel.local_balance)) {
-				// TODO fund existing channel functionality
-			}
-		}
-	}
+    if (validateAddress(address)) {
+      this.account.address = address
+      this.master._log.trace(`Successfully linked address ${address} to account ${this.account.accountName}`)
+    } else {
+      this.master._log.trace(`Failed to link address: ${address} is not a valid address`)
+    }
+  }
 
-	async attemptSettle () {
-	}
+  async fundOutgoingChannel(settlementBudget: BigNumber): Promise < BigNumber > {
+    let requiresNewChannel = false
+    let requiresDeposit = false
+    let channel: Channel | null
 
-	async handleData (message: BtpPacket, dataHandler?: DataHandler): Promise<BtpSubProtocol[]> {
-		return []
-	}
+    // Check if channel already exists
+    if (typeof (this.account.channel) === 'string') {
+      channel = await this._getChannel()
+      // if channel does not exist, just create a new one
+      if (!channel) return lndlib.createChannel()
+      if (settlementBudget.gt(channel.local_balance)) {
+        // TODO fund existing channel functionality
+      }
+    }
+  }
 
-	async handleMoney (message: BtpPacket, moneyHandler?: MoneyHandler): Promise<BtpSubProtocol[]> {
-		return []
-	}
+  async attemptSettle() {}
 
-	beforeForward (preparePacket: IlpPacket.IlpPacket): void { 
-	}
+  async handleData(message: BtpPacket, dataHandler ? : DataHandler): Promise < BtpSubProtocol[] > {
+    return []
+  }
 
-	async afterForwardResponse (preparePacket: IlpPacket.IlpPacket, responsePacket: IlpPacket.IlpPacket): Promise<void> {
-	}
+  async handleMoney(message: BtpPacket, moneyHandler ? : MoneyHandler): Promise < BtpSubProtocol[] > {
+    return []
+  }
 
-	async disconnect () { 
-	}
+  beforeForward(preparePacket: IlpPacket.IlpPacket): void {}
+
+  async afterForwardResponse(preparePacket: IlpPacket.IlpPacket, responsePacket: IlpPacket.IlpPacket): Promise < void > {}
+
+  async disconnect() {}
 }
--- src/utils/lndlib.ts
+++ src/utils/lndlib.ts
@@ -7,76 +7,76 @@ const lnrpc = grpc.load('rpc.proto').lnrpc
 process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
 
 export default class LndLib {
-	/* FIXME currently we don't need the constructor to do
-	 * anything because the actual initialization is async, 
-	 * there is def. a cleaner way to do this than just having 
-	 * an empty constructor then having a .connect() step */
-	private lightning: any
-	constructor(){}
+  /* FIXME currently we don't need the constructor to do
+   * anything because the actual initialization is async, 
+   * there is def. a cleaner way to do this than just having 
+   * an empty constructor then having a .connect() step */
+  private lightning: any
+  constructor() {}
 
-	async connect() {
+  async connect() {
 
-		// Default lnd host and port
-		const lndHost: string = 'localhost:10009'
+    // Default lnd host and port
+    const lndHost: string = 'localhost:10009'
 
-		// get ssl certificate
-		const lndCert = await this._getCert()
+    // get ssl certificate
+    const lndCert = await this._getCert()
 
-		// macaroon credentials
-		const macaroonCreds = await this._getMacaroonCreds()
-		
-		// combine credentials for lnrpc
-		const sslCreds = grpc.credentials.createSsl(lndCert)
-		const combinedCredentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds)
+    // macaroon credentials
+    const macaroonCreds = await this._getMacaroonCreds()
 
-		// create lightning instance
-		this.lightning = new lnrpc.Lightning(lndHost, combinedCredentials)
-	}
+    // combine credentials for lnrpc
+    const sslCreds = grpc.credentials.createSsl(lndCert)
+    const combinedCredentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds)
 
-	/******************* Calls to lightning daemon *****************/
+    // create lightning instance
+    this.lightning = new lnrpc.Lightning(lndHost, combinedCredentials)
+  }
 
-	async getInfo(): Promise<void> {
-		const resp = await this.lightning.getInfo()
-		console.log(resp)
-	}
+  /******************* Calls to lightning daemon *****************/
 
-	// TODO what will this return type be?
-	createChannel(peerPubKey: string, fundingAmt: number, pushAmt: number): Promise<void> {
-		// if is peer already:
-		const request: Object = {
-			node_pubkey_string: peerPubKey,
-			local_funding_amt: fundingAmt,
-			push_sat: pushAmt
-		}
-		const call = this.lightning.openChannel(request)
-		call.on('data', function(response: string) {
-			console.log(response)
-		})
-		call.on('status', function(status: string) {
-			console.log(status)
-		})
-		call.on('end', function() {
-			console.log(`Channel of amount ${fundingAmt} opened with peer ${peerPubKey}`)
-		})
-	}
+  async getInfo(): Promise < void > {
+    const resp = await this.lightning.getInfo()
+    console.log(resp)
+  }
 
-	async isPeer(peerIdentityPubKey: string): <boolean> {
-		// checks if at least one peer w/ peer identity pubkey existsexists
-		const peers: any[] = await listPeers()
-		return peers.some(p => p.pub_key === peerIdentityPubKey)
-	}
+  // TODO what will this return type be?
+  createChannel(peerPubKey: string, fundingAmt: number, pushAmt: number): Promise < void > {
+    // if is peer already:
+    const request: Object = {
+      node_pubkey_string: peerPubKey,
+      local_funding_amt: fundingAmt,
+      push_sat: pushAmt
+    }
+    const call = this.lightning.openChannel(request)
+    call.on('data', function (response: string) {
+      console.log(response)
+    })
+    call.on('status', function (status: string) {
+      console.log(status)
+    })
+    call.on('end', function () {
+      console.log(`Channel of amount ${fundingAmt} opened with peer ${peerPubKey}`)
+    })
+  }
 
-	async connectPeer(peerAddress: string, peerHost: string): Promise<string> {
-		// lnd requests this format for this call
-		const request: string = `${peerAddress}@${peerHost}`
-		return await this.lightning.connectPeer(request)
-	}
+  async isPeer(peerIdentityPubKey: string): < boolean > {
+    // checks if at least one peer w/ peer identity pubkey existsexists
+    const peers: any[] = await listPeers()
+    return peers.some(p => p.pub_key === peerIdentityPubKey)
+  }
 
-	async listPeers(): Promise<any[]> {
-		return await this.lightning.listPeers()
-	}
+  async connectPeer(peerAddress: string, peerHost: string): Promise < string > {
+    // lnd requests this format for this call
+    const request: string = `${peerAddress}@${peerHost}`
+    return await this.lightning.connectPeer(request)
+  }
 
-  async _getChannel (pub_key: string): Channel {
+  async listPeers(): Promise < any[] > {
+    return await this.lightning.listPeers()
+  }
+
+  async _getChannel(pub_key: string): Channel {
     const channels: Channel[] = this._lightning.listChannels()
     const filteredChannels: Channel[] = channels.channels.filter(c => c.remote_pubkey == pub_key)
 
@@ -93,66 +93,66 @@ export default class LndLib {
     }
   }
 
-	/******************* Lnd initialization helpers ****************/
+  /******************* Lnd initialization helpers ****************/
 
 
-	async _getCert (): Promise<string> {
-		/* Retrieve default path of tls.cert for lnd according
-		 * to operating system of the user */
-		const certPath: string = (function(operatingSystem) {
-			switch (operatingSystem) {
-				case 'darwin':
-					return `${process.env.HOME}/Library/Application Support/Lnd/tls.cert`
-				case 'linux':
-					return `${process.env.HOME}/.lnd/tls.cert`
-				// TODO unsure of path for windows, giving it same path as linux currently
-				case 'win32':
-					return `${process.env.HOME}/.lnd/tls.cert`
-				default:
-					throw new Error('In querying for lnd tls.cert path the OS does not match mac, linux or windows.')
-			}
-		})(os.process())
+  async _getCert(): Promise < string > {
+    /* Retrieve default path of tls.cert for lnd according
+     * to operating system of the user */
+    const certPath: string = (function (operatingSystem) {
+      switch (operatingSystem) {
+        case 'darwin':
+          return `${process.env.HOME}/Library/Application Support/Lnd/tls.cert`
+        case 'linux':
+          return `${process.env.HOME}/.lnd/tls.cert`
+          // TODO unsure of path for windows, giving it same path as linux currently
+        case 'win32':
+          return `${process.env.HOME}/.lnd/tls.cert`
+        default:
+          throw new Error('In querying for lnd tls.cert path the OS does not match mac, linux or windows.')
+      }
+    })(os.process())
 
-		/* try to access file in certPath, and throw error if file does not exist */
-		try {
-			return fs.readFileSync(certPath)
-		} catch (e) {
-			throw new Error('tls.cert does not exist in default location for this OS')
-		}
-	}
+    /* try to access file in certPath, and throw error if file does not exist */
+    try {
+      return fs.readFileSync(certPath)
+    } catch (e) {
+      throw new Error('tls.cert does not exist in default location for this OS')
+    }
+  }
 
-	// TODO change return type to macaroon meta
-	async _getMacaroonCreds(): Promise<any> {
-		const macaroonPath: string = (function(operatingSystem) {
-			switch (operatingSystem) {
-				case 'darwin':
-					return `${process.env.HOME}/Library/Application Support/Lnd/admin.macaroon` 
-				case 'linux':
-					return `${process.env.HOME}/.lnd/admin.macaroon`
-				// TODO unsure of path for windows, giving it same path as linux
-				case 'win32':
-					return `${process.env.HOME}/.lnd/admin.macaroon`
-				default:
-					throw new Error('Query for macaroonPath failed because OS does not match mac, linux or windows.')
-			}
-		})(os.process())
+  // TODO change return type to macaroon meta
+  async _getMacaroonCreds(): Promise < any > {
+    const macaroonPath: string = (function (operatingSystem) {
+      switch (operatingSystem) {
+        case 'darwin':
+          return `${process.env.HOME}/Library/Application Support/Lnd/admin.macaroon`
+        case 'linux':
+          return `${process.env.HOME}/.lnd/admin.macaroon`
+          // TODO unsure of path for windows, giving it same path as linux
+        case 'win32':
+          return `${process.env.HOME}/.lnd/admin.macaroon`
+        default:
+          throw new Error('Query for macaroonPath failed because OS does not match mac, linux or windows.')
+      }
+    })(os.process())
 
-		try {
-			const macaroonFile: any = fs.readFileSync(macaroonPath)
-			const macaroon: string = macaroonFile.toString('hex')
-			let meta = new grpc.Metadata()
-			meta.add('macaroon', macaroon)
-			return grpc.credentials.createFromMetadataGenerator((_args: any, callback: any) => {
-					callback(null, meta) })
-		} catch (e) {
-			throw new Error('admin.macaroon does not exist in default location for this OS')
-		}
-	}
-		/*
+    try {
+      const macaroonFile: any = fs.readFileSync(macaroonPath)
+      const macaroon: string = macaroonFile.toString('hex')
+      let meta = new grpc.Metadata()
+      meta.add('macaroon', macaroon)
+      return grpc.credentials.createFromMetadataGenerator((_args: any, callback: any) => {
+        callback(null, meta)
+      })
+    } catch (e) {
+      throw new Error('admin.macaroon does not exist in default location for this OS')
+    }
+  }
+  /*
 	async loadLnd(): Promise<any> {
 		let opts = {keepCase: true, longs: String, enums: String, defaults: true, oneofs: true}
 		const packageDefinition = await protoLoader.load(this.protoPath, opts)
 		const lnrpcDescriptor = grpc.loadPackageDefinition(packageDefinition)
 		 */
 }
-
