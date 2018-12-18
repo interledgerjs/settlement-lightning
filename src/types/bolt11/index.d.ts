1f1014647d34ddb25a3461ac283da601f41d547e
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2018-12-18 16:50:25 -0500

    fix: more accurate bolt11 types

--- src/types/bolt11/index.d.ts
+++ src/types/bolt11/index.d.ts
@@ -1,7 +1,20 @@
 /// <reference types="node" />
 
-// TODO Fix this!
 declare module 'bolt11' {
+  export type Tag =
+    | {
+        tagName: 'payment_hash'
+        data: string
+      }
+    | {
+        tagName: 'expire_time'
+        data: number
+      }
+    | {
+        tagName: string
+        data: any
+      }
+
   function decode(
     payReq: string
   ): {
@@ -13,10 +26,7 @@ declare module 'bolt11' {
     prefix: 'lnbc20m' | 'lntb20m' | 'lnbcrt20'
     recoveryFlag: 0 | 1 | 2 | 3
     signature: string
-    tags: {
-      tagName: string
-      data: any
-    }[]
+    tags: Tag[]
     timestamp: number
     timestampString: string
     wordsTemp: string
